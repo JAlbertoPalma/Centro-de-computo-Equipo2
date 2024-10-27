ad832b5ead3e300d074724c9517303eaf8193297
Brandon Valenzuela <brandon.valenzuela247077@potros.itson.edu.mx>
2024-10-27 16:58:43 -0700

    Clase Exception de la capa de negocio

---
+++ src/main/java/negocio/exception/NegocioException.java
@@ -0,0 +1,12 @@
+package negocio.exception;
+
+/**
+ *
+ * @author brand
+ */
+public class NegocioException extends Exception{
+    
+    public NegocioException(String mensaje) {
+        super(mensaje);
+    }
+}
